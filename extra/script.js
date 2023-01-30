03eaf6afecd2087a73c0210ad5e1839c34e62f31
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-01-30 14:08:01 +0100

    funcion que analiza la edad

---
+++ extra/script.js
@@ -0,0 +1,8 @@
+function edad(number){
+    if (number<=18){
+        alert.getElementById("edad").innerHTML="Eres menor de 18 años."
+    }
+    else if (number>100){
+        alert.getElementById("edad").innerHTML="Eres mayor de 100 años."
+    }
+}
