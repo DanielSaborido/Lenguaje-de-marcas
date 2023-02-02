d617cae349c2e8fb1e7709ee39cebe754d3641bd
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-02-02 12:59:48 +0100

    arreglos

--- relacion 2/java/ejercicio25.js
+++ relacion 2/java/ejercicio25.js
@@ -2,16 +2,16 @@ var nota=prompt("Escribe una nota")
 if (nota>=0,nota<4){
     alert("Insuficiente")
 }
-if (nota>=4,nota<5){
+else if (nota>=4,nota<5){
     alert("Suficiente")
 }
-if (nota>=5,nota<7){
+else if (nota>=5,nota<7){
     alert("Bien")
 }
-if (nota>=7,nota<=8){
+else if (nota>=7,nota<=8){
     alert("Notable")
 }
-if (nota>=9,nota<=10){
+else if (nota>=9){
     alert("Sobresaliente")
 }
 else{
--- relacion 2/java/ejercicio4.js
+++ relacion 2/java/ejercicio4.js
@@ -2,15 +2,15 @@ var nota=prompt("Escribe una nota")
 if (nota<4){
     alert("Insuficiente")
 }
-if (nota>=4,nota<5){
+else if (nota>=4,nota<5){
     alert("Suficiente")
 }
-if (nota>=5,nota<7){
+else if (nota>=5,nota<7){
     alert("Bien")
 }
-if (nota>=7,nota<=8){
+else if (nota>=7,nota<=8){
     alert("Notable")
 }
-if (nota>=9){
+else if (nota>=9){
     alert("Sobresaliente")
 }
