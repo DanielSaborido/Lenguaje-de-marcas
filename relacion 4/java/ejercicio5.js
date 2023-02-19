78d6fc6ee464b2aebc85a118b6e50b8b72f6aacd
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-02-19 15:34:59 +0100

    ejercicio 5

--- relacion 4/java/ejercicio5.js
+++ relacion 4/java/ejercicio5.js
@@ -1,3 +1,11 @@
-var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
+const dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
 
-document.getElementById("dias").innerHTML = fruits.toString()
+document.getElementById("dias").innerHTML = dias
+dias.reverse()
+document.getElementById("revertido").innerHTML = dias
+dias.sort()
+document.getElementById("alfabetico").innerHTML = dias
+for (dia in dias){
+    if (dias[dia]=='Lunes')
+    document.write("El Lunes esta en la posicion "+dia)
+}
