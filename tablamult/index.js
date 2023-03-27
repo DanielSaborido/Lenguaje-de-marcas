f1bdd513ad8af6537122faa8bfbb505ec2767a56
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-03-27 16:54:32 +0200

    fin de la calculadora

--- tablamult/index.js
+++ tablamult/index.js
@@ -1,8 +1,7 @@
 function multriplicacion(){
     var num = document.getElementById("num").value;
-    num=parseInt(num);
-    if (isNaN(num)){alert("El numero no es un entero.")}
-    else{
+    if (isNaN(num)) {alert("[ERROR] El valor introduciodo es un String.")}
+    else if (Number.isInteger(parseFloat(num))) {
         var tabla = document.getElementById("tabla");
         tabla.innerHTML = "";
         for (var i = 0; i <= 10; i++) {
@@ -19,6 +18,7 @@ function multriplicacion(){
             celda5.innerHTML = (num*i);
         }
 	}
+    else {alert("[ERROR] El valor introduciodo es un Float.")}
 }
 
 function reseteo(){
