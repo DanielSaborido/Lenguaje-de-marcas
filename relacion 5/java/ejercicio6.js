308e78b4b0b0f1d9a8e455f089da9981dc917585
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-04-11 21:27:03 +0200

    ej6 recortado y arreglado

--- relacion 5/java/ejercicio6.js
+++ relacion 5/java/ejercicio6.js
@@ -15,14 +15,14 @@ if (!nombre) {
     alert("Los apellidos no se introdujeron.");
 } else if (apellidos.split(" ").length > 2) {
     alert("En España solo hay 2 apellidos.");
-} else if (apellidos.split(" ").length = 1) {
-    alert("No introduciste el segundo apellido.");
 } else {
     const [apell1, apell2] = apellidos.split(" ");
     if (!validarNombre(nombre)) {
         alert("El nombre es muy corto o contiene caracteres especiales.");
     } else if (!validarApellido(apell1)) {
         alert("El primer apellido es muy corto o contiene caracteres especiales.");
+    } else if (!apell2) {
+        alert("El segundo apellido no se introdujo.");
     } else if (!validarApellido(apell2)) {
         alert("El segundo apellido es muy corto o contiene caracteres especiales.");
     } else {
