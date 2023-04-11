b2e881d630768c397a4ab722c7021673b3774ba4
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-04-11 21:12:36 +0200

    recorte de codigo

--- relacion 5/java/ejercicio6.js
+++ relacion 5/java/ejercicio6.js
@@ -1,41 +1,31 @@
 const nombre = prompt("Introduce tu nombre");
 const apellidos = prompt("Introduce tus apellidos separados por un espacio");
-const [apell1, apell2, apell3] = apellidos.split(" ");
+
+function validarNombre(nombre) {
+    return /^[a-zA-Z]+$/.test(nombre) && nombre.length >= 3;
+}
+
+function validarApellido(apellido) {
+    return /^[a-zA-Z]+$/.test(apellido) && apellido.length >= 3;
+}
 
 if (!nombre) {
     alert("El nombre no se introdujo.");
+} else if (!apellidos) {
+    alert("Los apellidos no se introdujeron.");
+} else if (apellidos.split(" ").length > 2) {
+    alert("En España solo hay 2 apellidos.");
+} else if (apellidos.split(" ").length = 1) {
+    alert("No introduciste el segundo apellido.");
 } else {
-    if (!apell2) {
-        alert("El segundo apellido no se introdujo.");
+    const [apell1, apell2] = apellidos.split(" ");
+    if (!validarNombre(nombre)) {
+        alert("El nombre es muy corto o contiene caracteres especiales.");
+    } else if (!validarApellido(apell1)) {
+        alert("El primer apellido es muy corto o contiene caracteres especiales.");
+    } else if (!validarApellido(apell2)) {
+        alert("El segundo apellido es muy corto o contiene caracteres especiales.");
     } else {
-        if (!apell3) {
-            if (apell1.length >= 3){
-                if (apell2.length >= 3) {
-                    if (nombre.length >= 3) {
-                        if (/^[a-zA-Z]+$/.test(apell1)) {
-                            if (/^[a-zA-Z]+$/.test(apell2)) {
-                                if (/^[a-zA-Z]+$/.test(nombre)) {
-                                    document.getElementById("saludo").innerHTML = `Hola ${apell1} ${apell2}, ${nombre}.`;
-                                } else {
-                                    alert("El nombre tiene caracteres especiales");
-                                }
-                            } else {
-                                alert("El segundo apellido tiene caracteres especiales");
-                            }
-                        } else {
-                            alert("El primer apellido tiene caracteres especiales");
-                        }
-                    } else {
-                        alert("El nombre es muy corto.");
-                    }
-                } else {
-                    alert("El segundo apellido es muy corto.");
-                }
-            } else{
-                alert("El primer apellido es muy corto.");
-            }
-        } else {
-            alert("En españa solo hay 2 apellidos.");
-        }
-    }    
+        document.getElementById("saludo").innerHTML = `Hola ${apell1} ${apell2}, ${nombre}.`;
+    }
 }
