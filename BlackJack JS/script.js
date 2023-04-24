908493edb659d22d62f3df6abdbf5f9a059ce750
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-04-24 20:10:06 +0200

    cambio de nombre

--- BlackJack JS/script.js
+++ BlackJack JS/script.js
@@ -1,13 +1,13 @@
 const baraja = []
 const jugadorDiv = document.getElementById('jugador')
-const dealerDiv = document.getElementById('dealer')
+const croupierDiv = document.getElementById('croupier')
 const PuntosJ = document.getElementById('Puntos-J')
 const PuntosD = document.getElementById('Puntos-D')
 const resultado = document.getElementById('resultado')
 const tusfichas = document.getElementById('tus-fichas')
 const fichasApostadas = document.getElementById('fichas-apostadas')
 const jugadorCartas = []
-const dealerCartas = []
+const croupierCartas = []
 var fichas = 1000
 var apuesta = 0
 tusfichas.innerHTML = `Tus fichas: ${fichas}`
@@ -17,7 +17,7 @@ fichasApostadas.innerHTML = `Tu apuesta actual: ${apuesta}`
 function crearBaraja() {
     baraja.splice(0, baraja.length)
     jugadorCartas.splice(0, jugadorCartas.length)
-    dealerCartas.splice(0, dealerCartas.length)
+    croupierCartas.splice(0, croupierCartas.length)
     const palos = ['picas', 'corazones', 'treboles', 'diamantes']
     const puntuaciones = ['As', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
@@ -46,17 +46,17 @@ function empezarPartida() {
     tusfichas.innerHTML = `Tus fichas: ${fichas}`
     resultado.innerHTML = ''
     jugadorCartas.push(baraja.shift(), baraja.shift())
-    dealerCartas.push(baraja.shift())
-    if (sumarPuntaje(dealerCartas) >= 10) {
-        dealerCartas.push(baraja.shift())
-        dealerDiv.innerHTML = `<h2>Cartas del Dealer</h2><ul><img src='cartas/${dealerCartas[0]}.png'><img src='cartas/${dealerCartas[1]}.png'></ul>`
+    croupierCartas.push(baraja.shift())
+    if (sumarPuntaje(croupierCartas) >= 10) {
+        croupierCartas.push(baraja.shift())
+        croupierDiv.innerHTML = `<h2>Cartas del Croupier</h2><ul><img src='cartas/${croupierCartas[0]}.png'><img src='cartas/${croupierCartas[1]}.png'></ul>`
     }
     jugadorDiv.innerHTML = `<h2>Cartas del Jugador</h2><ul><img src='cartas/${jugadorCartas[0]}.png'><img src='cartas/${jugadorCartas[1]}.png'></ul>`
     PuntosJ.innerHTML = `<h2>Puntuacion del Jugador: ${sumarPuntaje(jugadorCartas)}</h2>`
-    PuntosD.innerHTML = `<h2>Puntuacion del Dealer: ${sumarPuntaje(dealerCartas)}</h2>`
-    if (dealerCartas.length < 2) {
-        dealerDiv.innerHTML = `<h2>Cartas del Dealer</h2><ul><img src='cartas/${dealerCartas[0]}.png'></ul>`
-        dealerCartas.push(baraja.shift())
+    PuntosD.innerHTML = `<h2>Puntuacion del Croupier: ${sumarPuntaje(croupierCartas)}</h2>`
+    if (croupierCartas.length < 2) {
+        croupierDiv.innerHTML = `<h2>Cartas del Croupier</h2><ul><img src='cartas/${croupierCartas[0]}.png'></ul>`
+        croupierCartas.push(baraja.shift())
     }
     if (sumarPuntaje(jugadorCartas) == 21) {
         resultado.innerHTML = `Has ganado`
@@ -65,7 +65,7 @@ function empezarPartida() {
         tusfichas.innerHTML = `Tus fichas: ${fichas}`
         fichasApostadas.innerHTML = `Tu apuesta actual: ${apuesta}`
     }
-    else if (sumarPuntaje(dealerCartas) == 21) {
+    else if (sumarPuntaje(croupierCartas) == 21) {
         resultado.innerHTML = `Has perdido`
         apuesta = 0
         tusfichas.innerHTML = `Tus fichas: ${fichas}`
@@ -88,13 +88,13 @@ function tomarCarta() {
     jugadorCartas.push(cartaJ)
     const jugadorDiv = document.getElementById('jugador').getElementsByTagName('ul')[0]
     jugadorDiv.innerHTML += `<img src='cartas/${cartaJ}.png'>`
-    if (sumarPuntaje(dealerCartas) < 17){
-        dealerCartas.push(cartaD)
-        const dealerDiv = document.getElementById('dealer').getElementsByTagName('ul')[0]
-        dealerDiv.innerHTML += `<img src='cartas/${cartaD}.png'>`
+    if (sumarPuntaje(croupierCartas) < 17){
+        croupierCartas.push(cartaD)
+        const croupierDiv = document.getElementById('croupier').getElementsByTagName('ul')[0]
+        croupierDiv.innerHTML += `<img src='cartas/${cartaD}.png'>`
     }
     PuntosJ.innerHTML = `<h2>Puntuacion del Jugador: ${sumarPuntaje(jugadorCartas)}</h2>`
-    PuntosD.innerHTML = `<h2>Puntuacion del Dealer: ${sumarPuntaje(dealerCartas)}</h2>`
+    PuntosD.innerHTML = `<h2>Puntuacion del Croupier: ${sumarPuntaje(croupierCartas)}</h2>`
 
     if (sumarPuntaje(jugadorCartas) == 21) {
         resultado.innerHTML = `Has ganado`
@@ -109,13 +109,13 @@ function tomarCarta() {
         tusfichas.innerHTML = `Tus fichas: ${fichas}`
         fichasApostadas.innerHTML = `Tu apuesta actual: ${apuesta}`
     }
-    else if (sumarPuntaje(dealerCartas) == 21) {
+    else if (sumarPuntaje(croupierCartas) == 21) {
         resultado.innerHTML = `Has perdido`
         apuesta = 0
         tusfichas.innerHTML = `Tus fichas: ${fichas}`
         fichasApostadas.innerHTML = `Tu apuesta actual: ${apuesta}`
     }
-    else if (sumarPuntaje(dealerCartas) >= 22) {
+    else if (sumarPuntaje(croupierCartas) >= 22) {
         resultado.innerHTML = `Has ganado`
         fichas += apuesta * 2
         apuesta = 0
@@ -130,7 +130,7 @@ function plantarse() {
         alert('La partida no ha empezado.')
         return false
     }
-    resultado.innerHTML = `${determinarGanador(sumarPuntaje(jugadorCartas), sumarPuntaje(dealerCartas))}`
+    resultado.innerHTML = `${determinarGanador(sumarPuntaje(jugadorCartas), sumarPuntaje(croupierCartas))}`
 }
 
 // Función para sumar el valor de las cartas y obtener un puntaje
@@ -157,34 +157,34 @@ function sumarPuntaje(mano) {
 }
 
 // Establece las normas del BlackJack a partir de las puntuaciones obtenidas
-function determinarGanador(puntajeJugador = 0, puntajeDealer = 0) {
+function determinarGanador(puntajeJugador = 0, puntajecroupier = 0) {
     if (puntajeJugador > 21) {
         apuesta = 0
         tusfichas.innerHTML = `Tus fichas: ${fichas}`
         fichasApostadas.innerHTML = `Tu apuesta actual: ${apuesta}`
-        return 'Ganó el dealer'
+        return 'Ganó el croupier'
     }
-    else if (puntajeDealer > 21) {
+    else if (puntajecroupier > 21) {
         fichas += apuesta * 2
         apuesta = 0
         tusfichas.innerHTML = `Tus fichas: ${fichas}`
         fichasApostadas.innerHTML = `Tu apuesta actual: ${apuesta}`
         return 'Ganaste'
     }
-    else if (puntajeJugador > puntajeDealer) {
+    else if (puntajeJugador > puntajecroupier) {
         fichas += apuesta * 2
         apuesta = 0
         tusfichas.innerHTML = `Tus fichas: ${fichas}`
         fichasApostadas.innerHTML = `Tu apuesta actual: ${apuesta}`
         return 'Ganaste'
     }
-    else if (puntajeDealer > puntajeJugador) {
+    else if (puntajecroupier > puntajeJugador) {
         apuesta = 0
         tusfichas.innerHTML = `Tus fichas: ${fichas}`
         fichasApostadas.innerHTML = `Tu apuesta actual: ${apuesta}`
-        return 'Ganó el dealer'
+        return 'Ganó el croupier'
     }
-    else if (puntajeJugador = puntajeDealer) {
+    else if (puntajeJugador = puntajecroupier) {
         fichas += apuesta
         apuesta = 0
         tusfichas.innerHTML = `Tus fichas: ${fichas}`
