b7711263724d6c699a11bf5b6f913742f71c8437
DanielSaborido <dsaborio@iesrafaelalberti.es>
2023-05-09 10:09:35 +0200

    base del 6

--- relacion 7/java/ejercicio6.js
+++ relacion 7/java/ejercicio6.js
@@ -1,2 +1,54 @@
-var text = "Esta frase <br> tiene dos <br> saltos de linea"
-document.getElementById("oracion").innerHTML  = text;
+const resultado = document.getElementById('resultado')
+resultado.innerHTML = ''
+
+function Validacion() {
+    var nombre = document.getElementById("nombre").value;
+    var apellidos = document.getElementById("apellidos").value;
+    var calle = document.getElementById("calle").value;
+    var codigop = document.getElementById("codigop").value;
+    var edad = document.getElementById("edad").value;
+    var correo = document.getElementById("correo").value;
+    var opcion = document.getElementById("opciones").value;
+
+    if (nombre == "" || edad == "" || correo == "") {
+        alert("Faltan datos por introducirse.");
+        return false;
+    }
+
+    var letras = /^[A-Za-z]+$/;
+    if (!nombre.match(letras) || nombre.length < 3) {
+        alert("El nombre no es valido.");
+        return false;
+    }
+
+    letras = /^[A-Za-z]+( [A-Za-z]+)*$/;
+    if (!apellidos.match(letras) || apellidos.split(' ').some(a => a.length < 3)) {
+        alert("El apellido no es valido.");
+        return false;
+    }
+
+    if (codigop.length != 5 || isNaN(codigop)) {
+        alert("El codigo postal no es valido.");
+        return false;
+    }
+
+    if (edad < 0) {
+        alert("La edad no es valida.");
+        return false;
+    }
+
+    var mail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if (!correo.match(mail)){
+        alert("Correo mal introducido.");
+        return false;
+    }
+
+    if (opcion == "Elegir") {
+        alert("No has modificado el select.");
+        return false;
+    }
+
+    resultado.innerHTML = "Formulario enviado correctamente.";
+    alert("Formulario enviado correctamente.");
+    return true;
+}
